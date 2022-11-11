4dfb844b9472f0f26edc080427242bb0fcf75806
JoseReza <josesotoreza@gmail.com>
2022-11-10 18:12:46 -0700

    First production deploy in raspberry pi

--- principalServer/start.sh
+++ principalServer/start.sh
@@ -1,1 +1,5 @@
-sudo /usr/bin/lxterminal --command="npm run start" --working-directory="/home/pi/Desktop/labVirtual/principalServer"
+
+cd /home/pi/Desktop/labVirtual/principalServer
+npm run start &
+/home/pi/Desktop/labVirtual/ngrok-arm http 3000
+#sudo /usr/bin/lxterminal --command="npm run start" 
