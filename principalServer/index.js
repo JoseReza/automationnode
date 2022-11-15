11d9f66dbd3638e0c57dfa56ba065b1458a865af
JoseReza <josesotoreza@gmail.com>
2022-11-14 19:07:46 -0700

    Fixed import bootstrap

--- principalServer/index.js
+++ principalServer/index.js
@@ -176,7 +176,7 @@ app.post("/data", login.check, async (req, res) => {
           <meta charset="UTF-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="stylesheet" href="bootstrap.css" />
+          <link rel="stylesheet" href="./../bootstrap.css" />
           <link rel="icon" href="./../icon.png" />
           <title>${req.body.template.name}</title>
         </head>
